98501eb7f4a0ed34d16e9fbb121392803e3d5c52
keicyKOh1Ca <74536272+keicyKOh1Ca@users.noreply.github.com>
2022-11-08 16:57:40 +0900

    Create LoginCheck.java

---
+++ java/javaee/loginckecker/LoginCheck.java
@@ -0,0 +1,15 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.interceptor.InterceptorBinding;
+
+/**
+ * ログインチェック用@インターフェース<br>
+ */
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.TYPE})
+public @interface LoginCheck{}
